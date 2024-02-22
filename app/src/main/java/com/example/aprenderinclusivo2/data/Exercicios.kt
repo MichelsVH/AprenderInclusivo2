9085f859f1d773844ec0d5d392ef54742665aa15
MichelsVH <miguelvh190@gmail.com>
2024-02-22 12:08:15 +0000

    v2

--- app/src/main/java/com/example/aprenderinclusivo2/data/Exercicios.kt
+++ app/src/main/java/com/example/aprenderinclusivo2/data/Exercicios.kt
@@ -3,8 +3,17 @@ package com.example.aprenderinclusivo2.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "tabela_exercicios")
+@Entity(tableName = "exercicios_animais")
 data class Exercicios(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val nome: String,
+    val numLetras: Int,
+    val imagem: String
+)
+@Entity(tableName = "exercicios_cores")
+data class Exercicios_animais(
 
     @PrimaryKey(autoGenerate = true)
     val id: Int,
